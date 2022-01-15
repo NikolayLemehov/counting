13f8bfafbef0c66a73a5dc041fa2eb168a1db31b
NikolayLiemiekhov <nicklemster@gmail.com>
2022-01-15 17:29:24 +0200

    Remove unnecessary code

--- src/components/output/Output.jsx
+++ src/components/output/Output.jsx
@@ -4,8 +4,8 @@ import classes from "./Output.module.css";
 const Output = ({usd}) => {
   return (
       <div className={classes.wrapper}>
-        <div className={classes.lead}>Usd</div>
-        <div className={classes.data}>{usd}</div>
+        <div>Usd</div>
+        <div>{usd}</div>
       </div>
   );
 };
